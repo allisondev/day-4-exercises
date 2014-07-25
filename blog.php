ed6b87c9af24ae6164c64aa7c3955a8305d3e0ab
Allison Law <hsallisonlaw@gmail.com>
2014-07-25 12:35:32 +0000

    Modified blog.php to read articles from database instead of from an array

--- blog.php
+++ blog.php
@@ -2,33 +2,27 @@
 
 <?php
 
-$articles = array();
+//Connect to MySQL
+$connection = mysql_connect('localhost', 'root', '');
 
-$article = new stdClass();
-$article->name = 'Frosting Adventures';
-$article->author = 'Mr.Frosting';
-$article->body = '<p>Applicake jujubes tart toffee unerdwear.com gummies powder jelly beans toffee. Donut brownie marzipan toffee liquorice. Cupcake bear claw dragée unerdwear.com sugar plum.</p>';
-$articles[] = $article;
+//Select 'day_4_exercises')
+mysql_select_db('day_4_exercises');
 
-$article = new stdClass();
-$article->name = 'Sprinkles Craze';
-$article->author = 'Missy Sprinkles';
-$article->body = '<p>Tootsie roll muffin chocolate cake sugar plum marzipan candy cake jelly-o macaroon. Toffee gingerbread carrot cake macaroon
-brownie. Gummi bears gummi bears chocolate sugar plum unerdwear.com cookie carrot cake lollipop gummies. Pudding danish chocolate cake marzipan carrot cake pudding sugar plum cake gingerbread.</p>';
-$articles[] = $article;
+//Check Connection
+if(!$connection){
+  die('Failed to connect to MySQL: ' . mysql_connect_error());
+}
 
-$article = new stdClass();
-$article->name = 'What\'s Your Flavor?';
-$article->author = 'Vanilla Ice';
-$article->body = '<p>Candy canes caramels fruitcake croissant macaroon carrot cake. Bear claw chocolate cake oat cake powder soufflé wafer candy pastry dessert. Gummies lollipop pie jujubes gingerbread tiramisu applicake.</p>';
-$articles[] = $article;
+//Get result from the 'articles' table
+$result = mysql_query("SELECT title, author, body FROM articles");
 
-foreach ($articles as $article){
-    print '<article>';
-    print '<h2>' . $article->name . '</h2>';
-    print '<em>By: ' . $article->author . '</em><br/>';
-    print $article->body;
-    print '</article>';
+//Loop through each row in the 'articles' table
+while($row = mysql_fetch_array($result)){
+  print '<article>';
+  print '<h2>' . $row['title'] . '</h2>';
+  print '<em>By: ' . $row['author']  . '</em>';
+  print $row['body'];
+  print'</article>';
 }
 
 ?>
